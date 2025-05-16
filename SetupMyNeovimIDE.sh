baad1c2bb8a71ad09912bd6fac785e4243ead53d
GameOver <sasha2k62024@gmail.com>
2025-05-16 09:54:29 +0300

    Updated a SetUP script!

--- SetupMyNeovimIDE.sh
+++ SetupMyNeovimIDE.sh
@@ -3,19 +3,19 @@
 shopt -s expand_aliases
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	sudo apt update && sudo apt upgrade && sudo apt install build-essential gdb wget shellcheck shfmt
+	sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential gdb wget shellcheck shfmt pkg-config lsb-release software-properties-common gnupg libfontconfig-dev
 
-	# Download and install fnm:
-	curl -o- https://fnm.vercel.app/install | bash
+	if ! command -v npm 2>&1 >/dev/null; then
+		# Download and install fnm:
+		curl -o- https://fnm.vercel.app/install | bash
 
-	# Download and install Node.js:
-	fnm install 22
+		# Download and install Node.js:
+		fnm install 22
 
-	# Verify the Node.js version:
-	node -v # Should print "v22.15.1".
+		source $HOME/.bashrc
 
-	# Verify npm version:
-	npm -v # Should print "10.9.2".
+		rm -rf 1
+	fi
 
 	npm i -g bash-language-server
 
@@ -23,13 +23,22 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 		sudo rm -rf /opt/nvim
 		sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+		echo export PATH="$PATH:/opt/nvim-linux-x86_64/bin" >>$HOME/.bashrc
+
+		rm -rf 1
 	fi
 
 	if ! command -v cargo 2>&1 >/dev/null; then
 		curl https://sh.rustup.rs -sSf | sh -s -- -y
-		if ! command -v alacritty 2>&1 >/dev/null; then
-			cargo install alacritty
-		fi
+		. "$HOME/.cargo/env"
+
+		rm -rf 1
+	fi
+
+	if ! command -v alacritty 2>&1 >/dev/null; then
+		cargo install alacritty
+
+		rm -rf 1
 	fi
 
 	if [ -d "$XDG_CONFIG_HOME/nvim/" ]; then
@@ -55,18 +64,18 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 		cp .alacritty.toml $HOME
 	fi
 
-	if ! command -v clangd-21 2 &>1 >/dev/null; then
+	if ! command -v clangd-21 2>&1 >/dev/null; then
 		wget https://apt.llvm.org/llvm.sh
 		chmod +x llvm.sh
 		sudo ./llvm.sh 21
+
+		rm -rf 1
 	fi
 
-	cmd=alias alacritty="env WAYLAND_DISPLAY= alacritty"
+	echo alias alacritty="env WAYLAND_DISPLAY= alacritty" >>$HOME/.bashrc
+	source ~/.bashrc
 
-	if ! grep -Fxq $cmd $HOME/.bashrc; then
-		echo $cmd >>$HOME/.bashrc
-		source ~/.bashrc
-	fi
+	curl -fsSL https://xmake.io/shget.text | bash
 
 	alacritty -e nvim
 fi
